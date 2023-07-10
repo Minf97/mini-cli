014f48c8915d87ebb2464a249cfabb445d711b55
Minf97 <287728237@qq.com>
2023-07-10 10:00:02 +0800

    feat: 5.1 ConfigTransform

---
+++ test/test.js
@@ -0,0 +1,10 @@
+
+const ConfigTransform = require('../lib/ConfigTransform.js')
+
+const vueConfig = new ConfigTransform({
+    js: ['vue.config.js']
+})
+
+console.log(vueConfig.transform({
+    text: '这是一个测试'
+}));
